4f2238dee443b184f6422efa474c2d24e0f17087
deepika.pareek <deepika.pareek@sanskrut.in>
2024-01-11 11:05:31 +0200

    get mongodb url from .env

--- util/database.js
+++ util/database.js
@@ -1,10 +1,11 @@
+require('dotenv').config(); 
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
 const mongoConnect = callback => {
-    MongoClient.connect('mongodb+srv://deepikapareek:46WlkFSM3G8Shcjw@cluster0.5uuiteq.mongodb.net/shop?retryWrites=true&w=majority').then(client =>{
+    MongoClient.connect(process.env.MONGODB_CONNECT_URL).then(client =>{
         console.log("connected")
         _db = client.db();
         callback();
