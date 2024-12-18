a9283d023b68cd60a533cb80c961d267bb1f2010
deepika1115 <deepikapareek91@gmail.com>
2024-12-18 16:38:28 +0200

    delete redundate files

--- util/controllers/error.js
+++
@@ -1,6 +0,0 @@
-exports.get404 = (req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
-};
-exports.get500 = (error, req, res, next) => {
-  res.status(500).render('500', { pageTitle: 'Server Error', path: '/500', error : error });
-};
